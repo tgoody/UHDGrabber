846ea27faa4a130b24dc1cf589c5f79d0b95217e
tgoody <tylergoodman07@yahoo.com>
2022-02-13 14:42:37 -0500

    first commit

---
+++ UHDGrabber/Models/LocalMovieContainer.cs
@@ -0,0 +1,11 @@
+namespace UHDGrabber.Data;
+
+public class LocalMovieContainer
+{
+    private string _movieName;
+    private string _imdbId;
+    private List<string> _magnetLinks;
+
+
+
+}
---
+++ UHDGrabber/Services/RadarrGrabberService.cs
@@ -0,0 +1,7 @@
+namespace UHDGrabber.Services;
+
+public class RadarrGrabberService
+{
+    private string _radarrApiKey;
+    private string _radarrIp;
+}
